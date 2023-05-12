83661aa55b9c51063758eb42690eda88161cd84a
awsivGit <viswanathan66@gmail.com>
2023-05-12 17:19:42 +0800

    Updates

---
+++ precache-manifest.9e4033573aa1fb0cfcafce658a9f7578.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b3dee2e26556b837d4d",
+    "url": "/static/css/main.d8fd083a.chunk.css"
+  },
+  {
+    "revision": "3b3dee2e26556b837d4d",
+    "url": "/static/js/main.3b3dee2e.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "a52e0b1b747ee1b041f9ba30e8099bd9",
+    "url": "/index.html"
+  }
+];
